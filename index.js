fbd783dadf4a8a78ddc32bdf65467c0af81d0a3c
K1ng of Queen <72661500+S0m0g0nOwO@users.noreply.github.com>
2021-05-03 16:18:23 +0400

    Adding Events

--- index.js
+++ index.js
@@ -19,6 +19,24 @@ client.once("ready", () => {
   client.user.setPresence({activity: {name: `.хелп`}, status: 'idle', type: 'WATCHING'});
 });
 
+fs.readdir('./events/', (err, files) => { 
+    if (err) return console.error(err); 
+    files.forEach(file => {
+        const eventFunction = require(`./events/${file}`); 
+        if (eventFunction.disabled) return; 
+
+        const event = eventFunction.event || file.split('.')[0]; 
+        const emitter = (typeof eventFunction.emitter === 'string' ? client[eventFunction.emitter] : eventFunction.emitter) || client; 
+        const once = eventFunction.once; 
+
+        try {
+            emitter[once ? 'once' : 'on'](event, (...args) => eventFunction.run(...args));
+        } catch (error) {
+            console.error(error.stack); 
+        }
+    });
+});
+
 
 client.on("message", async message => {
 
